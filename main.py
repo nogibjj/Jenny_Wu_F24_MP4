cda88931e3986f4d1f195e2832e4671ab876e84d
Jenny <wujenny214@gmail.com>
2024-09-24 22:10:34 -0400

    adding markdown code

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ def load_and_preprocess(csv):
 def get_summary_stats(general_df, col):
     """function that calls for the summary statistics for the variable age_years"""
     desc_stats = general_df[col].describe()
-    print(
-        f'The average age of a Congress member from during a  {round(desc_stats["mean"], 3)}'
-    )
-    print(
-        f'The median age of a Congress member from during a {round(desc_stats["50%"], 3)}'
-    )
-    print(
-        f'Standard Deviation between the ages of Congress members is {round(desc_stats["std"], 3)}'
-    )
-
     return desc_stats
 
 
@@ -40,7 +30,44 @@ def hist_cong_age(general_df, col):
     plt.xlabel("Age", fontsize=14)
     plt.ylabel("Frequency", fontsize=14)
     plt.savefig("output/congressional_age.png")
-    plt.show()
+    # plt.show()
+
+
+def age_dist_50(general_df, desc_stats):
+    """builds a bar graph that demonstrates distribution of age cross chambers"""
+    # Convert 'start_date' column to datetime format
+    general_df["start_date"] = pd.to_datetime(general_df["start_date"])
+
+    # Bin the ages into 5-year intervals
+    general_df["age_group"] = pd.cut(
+        general_df["age_years"], bins=range(20, 101, 5), right=False
+    )
+
+    # Group by chamber and age group to get frequency counts
+    chamber_age_group_counts = (
+        general_df.groupby(["chamber", "age_group"], observed=False)
+        .size()
+        .unstack(fill_value=0)
+    )
+    # .size().unstack(fill_value=0)
+    # Plot a bar graph for each chamber's age distribution (with binned ages)
+    chamber_age_group_counts.T.plot(kind="bar", figsize=(12, 8), stacked=False)
+    plt.title("Age Distribution by Chamber in Congress (Binned)")
+    plt.xlabel("Age Group (Years)")
+    plt.ylabel("Frequency")
+    plt.legend(title="Chamber", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.tight_layout()
+    plt.savefig("output/chamber_age.png")
+    # plt.show()
+
+    stat_md = desc_stats.to_markdown()
+    with open("cong_age_summary.md", "w", encoding="utf-8") as file:
+        file.write("Describe:\n")
+        file.write(stat_md)
+        file.write("\n\n")
+        file.write("![congressional_age](/output/congressional_age.png)\n")
+        file.write("\n\n")
+        file.write("![chamber_age](output/chamber_age.png)\n")
 
 
 def main():
@@ -48,11 +75,11 @@ def main():
     csv = "https://raw.githubusercontent.com/fivethirtyeight/data/master/congress-demographics/data_aging_congress.csv"
 
     general_df = load_and_preprocess(csv)
-    s_stats = get_summary_stats(general_df, "age_years")
+    desc_stats = get_summary_stats(general_df, "age_years")
     hist_cong_age(general_df, "age_years")
-
+    age_dist_50(general_df, desc_stats)
     # print(general_df.head())
-    print(s_stats)
+    print(desc_stats)
 
 
 if __name__ == "__main__":
